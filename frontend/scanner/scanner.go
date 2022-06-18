ed427dccee49403ed977bd0ade2cd6cc92efc187
hotfireeagle <hotfireeagle@163.com>
2022-06-18 18:50:51 +0800

    feat: scanner temp save

--- frontend/scanner/scanner.go
+++ frontend/scanner/scanner.go
@@ -1,4 +1,48 @@
 package scanner
 
+import (
+	"jsj/frontend/source"
+	"jsj/frontend/token"
+)
+
 type Scanner struct {
+	// source, which is code file
+	source source.Source
+
+	// current token
+	token token.Token
+}
+
+// Scanner Object Constructor
+func ScannerConstructor(s source.Source) *Scanner {
+	return &Scanner{
+		source: s,
+	}
+}
+
+// return current token
+func (scannerInstance *Scanner) CurrentToken() token.Token {
+	return scannerInstance.token
+}
+
+// return next token from the source
+func (scannerInstance *Scanner) NextToken() token.Token {
+	nt := scannerInstance.extractToken()
+	return nt
+}
+
+// extract : 提取
+// do the actual work of extracting token and return the next token from the source
+func (scannerInstance *Scanner) extractToken() token.Token {
+	return 1
+}
+
+// return the current character from the source
+func (scannerInstance *Scanner) CurrentChar() byte {
+	return scannerInstance.source.CurrentChar()
+}
+
+// return the next character from the source
+func (scannerInstance *Scanner) NextChar() byte {
+	return scannerInstance.source.NextChar()
 }
